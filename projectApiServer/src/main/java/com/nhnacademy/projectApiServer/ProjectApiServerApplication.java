618092487934ab43cc3ac507f2e48a81f4d0d76b
Dodari <whdgus0515@gmail.com>
2022-06-12 21:13:34 +0900

    project api init

---
+++ projectApiServer/src/main/java/com/nhnacademy/projectApiServer/ProjectApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.projectApiServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiServerApplication.class, args);
+	}
+
+}
---
+++ projectApiServer/src/test/java/com/nhnacademy/projectApiServer/ProjectApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.projectApiServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
