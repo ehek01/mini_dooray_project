d3490f589c93e087fed3caa82c3e3ccdc40ff11a
Dodari <whdgus0515@gmail.com>
2022-06-12 18:58:40 +0900

    account apu init

---
+++ account_api_server/src/test/java/com/nhnacademy/account_api_server/AccountApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.account_api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
